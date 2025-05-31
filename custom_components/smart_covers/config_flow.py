b5c9c9b0042ea66198ce3db6862f03c71a46049c
Eyal Michon <eyalmichon1@gmail.com>
2025-05-31 13:13:59 +0300

    Refactor Smart Covers configuration flow to streamline data schema creation by consolidating repetitive code. Ensure default values are correctly set for all parameters, enhancing readability and maintainability.

--- custom_components/smart_covers/config_flow.py
+++ custom_components/smart_covers/config_flow.py
@@ -143,270 +143,95 @@ class SmartCoversOptionsFlow(config_entries.OptionsFlow):
 
         data_schema_dict = {}
 
-        # 1) ent_name
-        data_schema_dict[
-            vol.Required(
-                "ent_name",
-                default=self.config_entry.data.get("ent_name", ""),
-            )
-        ] = str
+        data_schema_dict[vol.Required("ent_name", default=self.config_entry.data.get("ent_name", ""))] = str
 
-        # 2) entity_up
-        data_schema_dict[
-            vol.Required(
-                "entity_up",
-                default=self.config_entry.data["entity_up"],
-            )
-        ] = selector.EntitySelector(
+        data_schema_dict[vol.Required("entity_up", default=self.config_entry.data["entity_up"])] = selector.EntitySelector(
             selector.EntitySelectorConfig(
                 domain=["switch", "input_boolean", "binary_sensor"],
                 multiple=False,
             )
         )
 
-        # 3) entity_down
-        data_schema_dict[
-            vol.Required(
-                "entity_down",
-                default=self.config_entry.data["entity_down"],
-            )
-        ] = selector.EntitySelector(
+        data_schema_dict[vol.Required("entity_down", default=self.config_entry.data["entity_down"])] = selector.EntitySelector(
             selector.EntitySelectorConfig(
                 domain=["switch", "input_boolean", "binary_sensor"],
                 multiple=False,
             )
         )
 
-        # 4) time_up, time_down, tilt_open, tilt_closed
-        data_schema_dict[
-            vol.Required(
-                "time_up",
-                default=self.config_entry.data.get("time_up", 0.0),
-            )
-        ] = vol.All(vol.Coerce(float), vol.Range(min=0))
-        data_schema_dict[
-            vol.Required(
-                "time_down",
-                default=self.config_entry.data.get("time_down", 0.0),
-            )
-        ] = vol.All(vol.Coerce(float), vol.Range(min=0))
-        data_schema_dict[
-            vol.Required(
-                "tilt_open",
-                default=self.config_entry.data.get("tilt_open", 0.0),
-            )
-        ] = vol.All(vol.Coerce(float), vol.Range(min=0))
-        data_schema_dict[
-            vol.Required(
-                "tilt_closed",
-                default=self.config_entry.data.get("tilt_closed", 0.0),
-            )
-        ] = vol.All(vol.Coerce(float), vol.Range(min=0))
+        data_schema_dict[vol.Required("time_up", default=self.config_entry.data.get("time_up", 0.0))] = vol.All(vol.Coerce(float), vol.Range(min=0))
+        data_schema_dict[vol.Required("time_down", default=self.config_entry.data.get("time_down", 0.0))] = vol.All(vol.Coerce(float), vol.Range(min=0))
+        data_schema_dict[vol.Required("tilt_open", default=self.config_entry.data.get("tilt_open", 0.0))] = vol.All(vol.Coerce(float), vol.Range(min=0))
+        data_schema_dict[vol.Required("tilt_closed", default=self.config_entry.data.get("tilt_closed", 0.0))] = vol.All(vol.Coerce(float), vol.Range(min=0))
 
-        # 5) timed_control_down / time_to_roll_down / timed_control_up / time_to_roll_up
-        data_schema_dict[
-            vol.Required(
-                "timed_control_down",
-                default=self.config_entry.data.get("timed_control_down", False),
-            )
-        ] = bool
-        data_schema_dict[
-            vol.Optional(
-                "time_to_roll_down",
-                default=self.config_entry.data.get("time_to_roll_down", "12:00"),
-            )
-        ] = vol.All(vol.Coerce(str))
-        data_schema_dict[
-            vol.Required(
-                "timed_control_up",
-                default=self.config_entry.data.get("timed_control_up", False),
-            )
-        ] = bool
-        data_schema_dict[
-            vol.Optional(
-                "time_to_roll_up",
-                default=self.config_entry.data.get("time_to_roll_up", "12:00"),
-            )
-        ] = vol.All(vol.Coerce(str))
 
-        # 6) delay_control / delay_sunrise / delay_sunset
-        data_schema_dict[
-            vol.Required(
-                "delay_control",
-                default=self.config_entry.data.get("delay_control", False),
-            )
-        ] = bool
-        data_schema_dict[
-            vol.Optional(
-                "delay_sunrise",
-                default=self.config_entry.data.get("delay_sunrise", 0),
-            )
-        ] = vol.All(vol.Coerce(int))
-        data_schema_dict[
-            vol.Optional(
-                "delay_sunset",
-                default=self.config_entry.data.get("delay_sunset", 0),
-            )
-        ] = vol.All(vol.Coerce(int))
+        data_schema_dict[vol.Required("timed_control_down", default=self.config_entry.data.get("timed_control_down", False))] = bool
+        data_schema_dict[vol.Optional("time_to_roll_down", default=self.config_entry.data.get("time_to_roll_down", "12:00"))] = vol.All(vol.Coerce(str))
 
-        # 7) night_lights + optional entity_night_lights
-        data_schema_dict[
-            vol.Required(
-                "night_lights",
-                default=self.config_entry.data.get("night_lights", False),
-            )
-        ] = bool
+        data_schema_dict[vol.Required("timed_control_up", default=self.config_entry.data.get("timed_control_up", False))] = bool
+        data_schema_dict[vol.Optional("time_to_roll_up", default=self.config_entry.data.get("time_to_roll_up", "12:00"))] = vol.All(vol.Coerce(str))
 
+
+        data_schema_dict[vol.Required("delay_control", default=self.config_entry.data.get("delay_control", False))] = bool
+        data_schema_dict[vol.Optional("delay_sunrise", default=self.config_entry.data.get("delay_sunrise", 0))] = vol.All(vol.Coerce(int))
+        data_schema_dict[vol.Optional("delay_sunset", default=self.config_entry.data.get("delay_sunset", 0))] = vol.All(vol.Coerce(int))
+
+        data_schema_dict[vol.Required("night_lights", default=self.config_entry.data.get("night_lights", False))] = bool
         existing_night = self.config_entry.data.get("entity_night_lights")
-        if existing_night:
-            data_schema_dict[
-                vol.Optional("entity_night_lights", default=existing_night)
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["light", "switch", "input_boolean"],
-                    multiple=False,
-                )
+        existing_night_key = vol.Optional("entity_night_lights", default=existing_night) if existing_night else vol.Optional("entity_night_lights")
+        data_schema_dict[existing_night_key] = selector.EntitySelector(
+            selector.EntitySelectorConfig(
+                domain=["light", "switch", "input_boolean"],
+                multiple=False,
             )
-        else:
-            data_schema_dict[
-                vol.Optional("entity_night_lights")
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["light", "switch", "input_boolean"],
-                    multiple=False,
-                )
-            )
+        )
 
-        # 8) tilting_day
-        data_schema_dict[
-            vol.Required(
-                "tilting_day",
-                default=self.config_entry.data.get("tilting_day", False),
-            )
-        ] = bool
+        data_schema_dict[vol.Required("tilting_day", default=self.config_entry.data.get("tilting_day", False))] = bool
 
-        # 9) protect_the_cover / wind_speed / wmo_code
-        data_schema_dict[
-            vol.Required(
-                "protect_the_cover",
-                default=self.config_entry.data.get("protect_the_cover", False),
-            )
-        ] = bool
-        data_schema_dict[
-            vol.Optional(
-                "wind_speed",
-                default=self.config_entry.data.get("wind_speed", 30),
-            )
-        ] = vol.All(vol.Coerce(float))
-        data_schema_dict[
-            vol.Optional(
-                "wmo_code",
-                default=self.config_entry.data.get("wmo_code", 80),
-            )
-        ] = vol.All(vol.Coerce(int))
+        data_schema_dict[vol.Required("protect_the_cover", default=self.config_entry.data.get("protect_the_cover", False))] = bool
 
-        # 10) netamo_enable / netamo_*_entity / netamo_* values
-        data_schema_dict[
-            vol.Required(
-                "netamo_enable",
-                default=self.config_entry.data.get("netamo_enable", False),
-            )
-        ] = bool
+        data_schema_dict[vol.Optional("wind_speed", default=self.config_entry.data.get("wind_speed", 30))] = vol.All(vol.Coerce(float))
+        data_schema_dict[vol.Optional("wmo_code", default=self.config_entry.data.get("wmo_code", 80))] = vol.All(vol.Coerce(int))
+
+        data_schema_dict[vol.Required("netamo_enable", default=self.config_entry.data.get("netamo_enable", False))] = bool
 
         existing_speed_ent = self.config_entry.data.get("netamo_speed_entity")
-        if existing_speed_ent:
-            data_schema_dict[
-                vol.Optional("netamo_speed_entity", default=existing_speed_ent)
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["sensor"],
-                    device_class=["wind_speed"],
-                    multiple=False,
-                )
+        existing_speed_ent_key = vol.Optional("netamo_speed_entity", default=existing_speed_ent) if existing_speed_ent else vol.Optional("netamo_speed_entity")
+        data_schema_dict[existing_speed_ent_key] = selector.EntitySelector(
+            selector.EntitySelectorConfig(
+                domain=["sensor"],
+                device_class=["wind_speed"],
+                multiple=False,
             )
-        else:
-            data_schema_dict[
-                vol.Optional("netamo_speed_entity")
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["sensor"],
-                    device_class=["wind_speed"],
-                    multiple=False,
-                )
-            )
+        )
 
-        data_schema_dict[
-            vol.Optional(
-                "netamo_speed",
-                default=self.config_entry.data.get("netamo_speed", 30),
-            )
-        ] = vol.All(vol.Coerce(float))
+        data_schema_dict[vol.Optional("netamo_speed", default=self.config_entry.data.get("netamo_speed", 30))] = vol.All(vol.Coerce(float))
 
         existing_gust_ent = self.config_entry.data.get("netamo_gust_entity")
-        if existing_gust_ent:
-            data_schema_dict[
-                vol.Optional("netamo_gust_entity", default=existing_gust_ent)
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["sensor"],
-                    device_class=["wind_speed"],
-                    multiple=False,
-                )
-            )
-        else:
-            data_schema_dict[
-                vol.Optional("netamo_gust_entity")
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
+        existing_gust_ent_key = vol.Optional("netamo_gust_entity", default=existing_gust_ent) if existing_gust_ent else vol.Optional("netamo_gust_entity")
+        data_schema_dict[existing_gust_ent_key] = selector.EntitySelector(
+            selector.EntitySelectorConfig(
                     domain=["sensor"],
                     device_class=["wind_speed"],
                     multiple=False,
                 )
             )
 
-        data_schema_dict[
-            vol.Optional(
-                "netamo_gust",
-                default=self.config_entry.data.get("netamo_gust", 40),
-            )
-        ] = vol.All(vol.Coerce(float))
+        data_schema_dict[vol.Optional("netamo_gust", default=self.config_entry.data.get("netamo_gust", 40))] = vol.All(vol.Coerce(float))
 
         existing_rain_ent = self.config_entry.data.get("netamo_rain_entity")
-        if existing_rain_ent:
-            data_schema_dict[
-                vol.Optional("netamo_rain_entity", default=existing_rain_ent)
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain=["sensor"],
-                    device_class=["precipitation"],
-                    multiple=False,
-                )
-            )
-        else:
-            data_schema_dict[
-                vol.Optional("netamo_rain_entity")
-            ] = selector.EntitySelector(
-                selector.EntitySelectorConfig(
+        existing_rain_ent_key = vol.Optional("netamo_rain_entity", default=existing_rain_ent) if existing_rain_ent else vol.Optional("netamo_rain_entity")
+        data_schema_dict[existing_rain_ent_key] = selector.EntitySelector(
+            selector.EntitySelectorConfig(
                     domain=["sensor"],
                     device_class=["precipitation"],
                     multiple=False,
                 )
             )
 
-        data_schema_dict[
-            vol.Optional(
-                "netamo_rain",
-                default=self.config_entry.data.get("netamo_rain", 40),
-            )
-        ] = vol.All(vol.Coerce(float))
+        data_schema_dict[vol.Optional("netamo_rain", default=self.config_entry.data.get("netamo_rain", 40))] = vol.All(vol.Coerce(float))
 
-        # 11) send_stop_at_end
-        data_schema_dict[
-            vol.Required(
-                "send_stop_at_end",
-                default=self.config_entry.data.get("send_stop_at_end", True),
-            )
-        ] = bool
+        data_schema_dict[vol.Required("send_stop_at_end", default=self.config_entry.data.get("send_stop_at_end", True))] = bool
 
         return self.async_show_form(
             step_id="init",
